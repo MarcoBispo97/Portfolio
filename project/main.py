a69a9fd07630f571865b41a7252ef5b7993094a6
Marco Bispo <marcobispo97@gmail.com>
2024-08-08 23:04:44 -0300

    projeto em andamento

--- project/main.py
+++ project/main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from deep_translator import GoogleTranslator
-
-# Função para traduzir textos
+from PIL import Image
 
 
 def tradutor(texto, traduzir):
@@ -11,66 +10,93 @@ def tradutor(texto, traduzir):
         return texto
 
 # Função para exibir o conteúdo da página selecionada
+language = st.sidebar.selectbox('Idiom/Idioma', ['🇺🇸 EN', '🇧🇷 PT'])
+traduzir_en = language == '🇺🇸 EN'
+
+def show_page(page_name, traduzir = traduzir_en):
+    st.title(tradutor("Portfolio Cientista de dados:", traduzir))
+    st.header(tradutor("Feito por: Marco A.Bispo", traduzir), divider=True)
+
+    if page_name == "📖 Menu":
+        st.write(tradutor("MENU !", traduzir))
 
 
-def show_page(page_name, traduzir):
-    st.title(tradutor("Portfolio Cientista de dados:\nMarco A.Bispo", traduzir))
-
-    if page_name == "navigation":
-        st.write(tradutor("Conteúdo da página de navegação", traduzir))
-    elif page_name == "data":
+    elif page_name == tradutor("📚 Dados",traduzir):
         st.write(tradutor("Conteúdo da página de dados", traduzir))
-    elif page_name == "exploratory_analysis":
+    elif page_name == tradutor("📊 Análise exploratória",traduzir):
         st.write(tradutor("Conteúdo da página de análise exploratória", traduzir))
-    elif page_name == "statistical_analysis":
+    elif page_name == tradutor("📈 Análise estatística",traduzir):
         st.write(tradutor("Conteúdo da página de análise estatística", traduzir))
-    elif page_name == "prediction_model":
+    elif page_name == tradutor("🧮 Modelagem",traduzir):
         st.write(tradutor("Conteúdo da página de modelo de previsão", traduzir))
-    elif page_name == "about_me":
-        st.write(
-            tradutor("Conteúdo da página sobre mim e onde me encontrar", traduzir))
+    elif page_name == tradutor("🔎 Sobre mim e onde me encontrar",traduzir):
+        
 
+        css_file = "C:\\Users\\marco\\Documents\\data_science\\Portfolio\\about_me\\main.css"
+        resume_file = "C:\\Users\\marco\\Documents\\data_science\\Portfolio\\about_me\\Marco Bispo c.v..pdf"
+        profile_pic = "C:\\Users\\marco\\Documents\\data_science\\Portfolio\\about_me\\profile-pic-official.png"
 
-def main():
-    global traduzir_en  # Declarando traduzir_en como global
+        # Abrindo e aplicando o arquivo CSS
+        with open(css_file) as f:
+            st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 
-    if 'traduzir_en' not in globals():  # Verifica se traduzir_en não está definido
-        traduzir_en = True  # Define traduzir_en como True por padrão
+        # Abrindo e lendo o arquivo PDF
+        with open(resume_file, "rb") as pdf_file:
+            PDFbyte = pdf_file.read()
 
-    # Selecionar idioma
-    language = st.sidebar.selectbox(
-        'Language', ['🇺🇸 EN', '🇧🇷 PT'])
+        # Abrindo e carregando a imagem de perfil
+        profile_pic = Image.open(profile_pic)
 
-    # Definir traduzir_en baseado na seleção de idioma
-    if language == '🇧🇷 PT':
-        traduzir_en = False
-    else:
-        traduzir_en = True
+        # Exibir o PDF no Streamlit
+        st.download_button(
+            label="Baixar Currículo",
+            data=PDFbyte,
+            file_name="Curriculo.pdf",
+            mime="application/pdf",
+        )
+        NAME = "Marco A.Bispo"
+        EMAIL = "marcobispo97@gmail.com"
+        SOCIAL_MEDIA = {
+            "Linkedin":"https://www.linkedin.com/in/marco-bispo-b66274150/",
+            "Github":"https://github.com/MarcoBispo97",
+            "Instagram":"https://www.instagram.com/marco_bispo/"
+        }
+        DESCRIPTION = tradutor("Cientista de dados, auxiliando empresas no suporte à tomada de decisões baseada em dados.",traduzir)
+        st.header(tradutor("🔎 Olá !", traduzir))
+        col1, col2 = st.columns(2, gap="small")
+        with col1:
+            st.image(profile_pic, width=230)
 
-    # Botões na barra lateral para navegação
-    if st.sidebar.button(tradutor("🧭 Navegação", traduzir_en)):
-        st.query_params(page="navigation")
-        show_page("navigation", traduzir_en)
+        with col2:
+            st.title(NAME)
+            st.write(DESCRIPTION)
+            st.download_button(
+                label=" 📄 Download Resume",
+                data=PDFbyte,
+                file_name="Marco_Bispo_CV.pdf",  # Substitua aqui pelo nome do arquivo desejado
+                mime="application/octet-stream",
+            )
+            st.write("📫", EMAIL)
 
-    if st.sidebar.button(tradutor("📚 Dados", traduzir_en)):
-        st.query_params(page="data")
-        show_page("data", traduzir_en)
+        st.write('\n')
+        cols = st.columns(len(SOCIAL_MEDIA))
+        for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
+            cols[index].write(f"[{platform}]({link})")
 
-    if st.sidebar.button(tradutor("📊 Análise exploratória", traduzir_en)):
-        st.query_params(page="exploratory_analysis")
-        show_page("exploratory_analysis", traduzir_en)
+def main(traduzir = traduzir_en):
+    st.sidebar.title(tradutor("🧭 Navegação", traduzir))
+    page = st.sidebar.radio(
+        tradutor("Escolha uma página", traduzir_en),
+        [tradutor('📖 Menu',traduzir),
+        tradutor("📚 Dados",traduzir),
+        tradutor("📊 Análise exploratória",traduzir),
+        tradutor("📈 Análise estatística",traduzir),
+        tradutor("🧮 Modelagem",traduzir),
+        tradutor("🔎 Sobre mim e onde me encontrar",traduzir)]
+        ,index=0
+    )
 
-    if st.sidebar.button(tradutor("📈 Análise estatística", traduzir_en)):
-        st.query_params(page="statistical_analysis")
-        show_page("statistical_analysis", traduzir_en)
-
-    if st.sidebar.button(tradutor("🧮 Modelo de previsão", traduzir_en)):
-        st.query_params(page="prediction_model")
-        show_page("prediction_model", traduzir_en)
-
-    if st.sidebar.button(tradutor("🔎 Sobre mim e onde me encontrar", traduzir_en)):
-        st.query_params(page="about_me")
-        show_page("about_me", traduzir_en)
+    show_page(page, traduzir_en)
 
 
 if __name__ == "__main__":
